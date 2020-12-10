37df20b67fdc009b2cec7b5dfe31edb0a7578de6
Bellomia <gbellomia@live.it>
2020-12-10 07:11:20 +0100

    Dry = Linspace in U without any automatized or manual control on anything
    Added:
    
    - "How to use?" section at head;
    - doMPI boolean flag to (de)activate openMPI parallelization
    - A noninvasive control on convergence: the U_list file will contain only converged U values (:

--- RunningDMFT.m/RunningDMFT_dry.m
+++ RunningDMFT.m/RunningDMFT_dry.m
@@ -1,3 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% HOW TO USE?
+%
+%  - Put this script, together with an input-file for your driver in a path
+%    > This path will contain directories for all the U values you set-up.
+%  - Set-up the name of your driver program (without .f90 extension)
+%    > e.g. driver = 'ed_kane_mele';
+%  - Adjust Umin, Uman and Ustep to your desire --> U = Umin:Ustep:Umax
+%  - Set SOI to your desire: --> you will get a fixed-SOI linear span
+%  - Adjust Uold to catch a 'restart-folder' in the path [!applies -> -1]
+%  - Select doMPI (true.or.false) to run with openMPI or not
+%  - Run everything with $ matlab -batch RunningDMFT_liveupdate
+%  - At the end you will find some additional output in the U=%f folders
+%    > a LOG_dmft.txt which is just a mirror of the DMFT output (via tee)
+%    > a LOG_time.txt which is a wall-clock-time value for the whole DMFT
+%  - Also an additional output file in the main (external) path
+%    > a U_list.txt that stores all the used U-values (for easier post..)
+%      NB. Only the converged calculations will update the U_list :D
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+driver = 'ed_kane_mele';	doMPI = false;
+
 % Let MATLAB see the goddamn PATH %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% -> works only if matlab has been started from a unix terminal! (0^0~~,)
 path = getenv('PATH');
@@ -5,7 +28,9 @@ path = [path ':/usr/local/bin'];
 setenv('PATH', path) 
 clear path
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 Ulist = fopen('U_list.txt','w');
+
 %% Phase-Line: single loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 SOI  = 0;                      % Input Spin-Orbit 
@@ -14,7 +39,7 @@ Ustep = 0.25;			% Phase-line step
 Uold = -1;			% Restart option
 
 U = Umin; 
-while U <= Umax                % Hubbard loop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+while U <= Umax                % Hubbard loop ~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 
 UDIR= sprintf('U=%f',U);       % Make a folder named 'U=...', where '...'
 mkdir(UDIR);                   % is the given value for Hubbard interaction
@@ -25,21 +50,29 @@ if isfolder(oldDIR)            % If it exist a "previous" folder:
 copyfile(oldDIR);              % Copy everything from last dmft evaluation
 end                            % -----------------------------------------
 
-copyfile ../inputKANEMELE.conf % Copy inside the *external* input file
+copyfile ../inputKANEMELE.conf % Copy inside the **external** input file
 
 %% Run FORTRAN code (already compiled and added to PATH!) %%%%%%%%%%%%%%%%%
-mpi = 'mpirun ';
-driver = 'ed_kane_mele';
-HUBBARD =sprintf(' uloc=%f',U);		% OVERRIDE of
-T2 =sprintf(' t2=%f',SOI);			% PARAMETERS
-OutLOG = ' > LOG.out';
-dmft_ed_call = [mpi,driver,HUBBARD,T2,OutLOG];
+if doMPI
+mpi = 'mpirun ';			        % Control of MPI
+else					        % boolean flag...
+mpi = [];
+end
+HUBBARD =sprintf(' uloc=%f',U);	        % OVERRIDE of
+T2 =sprintf(' t2=%f',SOI);		        % PARAMETERS
+outLOG = ' > LOG.out';
+dmft_ed_call = [mpi,driver,HUBBARD,T2,outLOG];
 tic
 system(dmft_ed_call);				% Fortran-call
 chrono = toc;
 file_id = fopen('LOG_time.txt','w');		% Write on time-log
 fprintf(file_id,'%f\n', chrono);
 fclose(file_id);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% HERE WE CATCH A FAILED (unconverged) DMFT LOOP
+if ~isfile('ERROR.README') 
+    fprintf(Ulist,'%f\n', U);	          % Update U-list (only if converged)
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 cd ..                          % Exit the U-folder
@@ -50,6 +83,8 @@ Uold = U;
 U = U + Ustep;              	% Hubbard update  
 
 end                            % <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 fclose(Ulist);
 
