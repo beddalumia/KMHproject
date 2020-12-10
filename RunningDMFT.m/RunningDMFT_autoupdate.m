788028e563a98cd4c26ed1e27298165bfbccf677
Bellomia <gbellomia@live.it>
2020-12-10 07:05:23 +0100

    Intermediate commit
    - Broken; work-in-progress

--- RunningDMFT.m/RunningDMFT_autoupdate.m
+++ RunningDMFT.m/RunningDMFT_autoupdate.m
@@ -1,3 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% HOW TO USE?
+%
+%  - Put this script, together with an input-file for your driver in a path
+%    > This path will contain directories for all the U values you set-up.
+%  - Set-up the name of your driver program (without .f90 extension)
+%    > e.g. driver = 'ed_kane_mele'; 
+%  - Adjust Umin, Uman and Ustep to your desire --> U=Umin:Ustep:Umax
+%  - Set SOI to your desire: --> you will get a fixed-SOI linear span
+%  - Adjust Uold to catch a 'restart-folder' in the path [!applies -> -1]
+%  - Adjust notConvThreshold to control how much DMFT will wait for you (!)
+%  - Select doMPI (true.or.false) to run with openMPI or not
+%  - Run everything with $ matlab -batch RunningDMFT_liveupdate
+%  - Once in a while control for the presence of ERROR.README in the path
+%    > If you see it, DMFT is not converging; you might want to inspect the
+%      the output files and update wmixing in the **external** input-file
+%      to a suitable value. At the next run DMFT will catch the update ;)
+%  - At the end you will find some additional output in the U=%f folders
+%    > a LOG_dmft.txt which is just a mirror of the DMFT output (via tee)
+%    > a LOG_time.txt which is a wall-clock-time value for the whole DMFT
+%  - Also an additional output file in the main (external) path
+%    > a U_list.txt that stores all the used U-values (for easier post..)
+%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+driver = 'ed_kane_mele';	doMPI = false;
+
 % Let MATLAB see the goddamn PATH %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% -> works only if matlab has been started from a unix terminal! (0^0~~,)
 path = getenv('PATH');
@@ -6,7 +33,9 @@ setenv('PATH', path)
 clear path
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Phase Line: single loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Ulist = fopen('U_list.txt','w');
+
+%% Phase-Line: single loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 SOI  = 0;                      % Input Spin-Orbit 
 Umin = 0; Umax = 10;           % Input Hubbard 
@@ -29,11 +58,14 @@ if isfolder(oldDIR)            % If it exist a "previous" folder:
 copyfile(oldDIR);              % Copy everything from last dmft evaluation
 end                            % -----------------------------------------
 
-copyfile ../inputKANEMELE.conf % Copy inside the *external* input file
+copyfile ../inputKANEMELE.conf % Copy inside the **external** input file
 
 %% Run FORTRAN code (already compiled and added to PATH!) %%%%%%%%%%%%%%%%%
-mpi = 'mpirun ';
-driver = 'ed_kane_mele';
+if doMPI
+mpi = 'mpirun ';				% Control of MPI
+else						% boolean flag...
+mpi = [];
+end
 HUBBARD =sprintf(' uloc=%f',U);		% OVERRIDE
 T2 =sprintf(' t2=%f',SOI);			% of
 MIXING = sprintf(' wmixing=%f',Wmix);		% PARAMETERS
@@ -46,13 +78,13 @@ file_id = fopen('LOG_time.txt','w');
 fprintf(file_id,'%f\n', chrono);
 fclose(file_id);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% HERE WE NEED TO CATCH SOMEHOW A FAILED (unconverged) DMFT LOOP
+%% HERE WE CATCH A FAILED (unconverged) DMFT LOOP
 if isfile('ERROR.README')
     notConverged = notConverged + 1;
     if Uold >= Umin
     U = Uold; % if nonconverged we don't want to update! 
     end
-    delete ERROR.README
+    movefile('ERROR.README','../ERROR.README');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -68,3 +100,7 @@ end
 U = U + Ustep(notConverged+1); % ...Hubbard update  
 
 end                            % <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fclose(Ulist);
