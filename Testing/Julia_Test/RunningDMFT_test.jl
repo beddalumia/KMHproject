1ba8717d00c6c990320d73118d95aa1c87f23260
Bellomia <gbellomia@live.it>
2020-12-18 12:18:13 +0100

    Update RunningDMFT_test.jl
    Finally solved the `| tee` problem...

--- Testing/Julia_Test/RunningDMFT_test.jl
+++ Testing/Julia_Test/RunningDMFT_test.jl
@@ -1,2 +1,26 @@
-run(`ed_kane_mele '|' tee log.out`)
-# tee apparently does not work, shit
+# run(`ed_kane_mele | tee LOG.dmft`) 
+# -> DOES NOT WORK: 
+#    ERROR: LoadError: LoadError: parsing command `ed_kane_mele | tee LOG.dmft`:
+#    special characters "#{}()[]<>|&*?~;" must be quoted in commands
+
+# run(`ed_kane_mele \| tee LOG.dmft`)
+# -> DOES NOT WORK: 
+#    Runs and all, but LOG.dmft never happens to exist...
+
+# run(`ed_kane_mele \> tee LOG.dmft`)
+# -> DOES NOT WORK: 
+#    Runs and all, but LOG.dmft never happens to exist...also stdout actually is printed on screen.
+
+# run(`ed_kane_mele > tee LOG.dmft`)
+# -> DOES NOT WORK: 
+#    ERROR: LoadError: LoadError: parsing command `ed_kane_mele | tee LOG.dmft`:
+#    special characters "#{}()[]<>|&*?~;" must be quoted in commands
+
+# run(`bash ed_kane_mele \| tee LOG.dmft`)
+# -> DOES NOT WORK: 
+#    ERROR: LoadError: failed process: 
+#    Process(`bash ed_kane_mele '|' tee LOG.dmft`, ProcessExited(126)) [126]
+
+run(`bash -c "ed_kane_mele | tee LOG.dmft" `)
+# -> Finally, this works.
+
