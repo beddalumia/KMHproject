efa757dd78f59d98cd3a2f091b9777154b44bd6e
Bellomia <gbellomia@live.it>
2021-01-17 20:11:30 +0100

    Proper behading of observables names

--- RunningDMFT.m/PostAnalysis/PlottingDMFT.m
+++ RunningDMFT.m/PostAnalysis/PlottingDMFT.m
@@ -79,5 +79,11 @@ function [names, observables] = get_observables()
 %  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     names = readcell('observables_info.ed','FileType','fixedwidth');
     names(strcmp(names,'#'))=[];
+    for i = 1:length(names)
+        tempstr = names{i};                 % Temporary string variable
+        head = sscanf(tempstr,'%d');        % Extracts the initial integer
+        head = int2str(head);               % Int to Str conversion
+        names{i} = erase(tempstr,head);     % Proper beheading ;D
+    end
     observables = load('observables_last.ed');
 end
