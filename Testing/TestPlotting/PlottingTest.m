061195c75a99a4567459eb3ad2a018ae44f8a7da
Bellomia <gbellomia@live.it>
2021-01-18 09:10:36 +0100

    Update PlottingTest.m
    + Phase Lines
    + One-channel Phase Diagrams
    + Spin-resolved Phase Diagrams (hardly useful..)
    
    Everything carefully tested

--- Testing/TestPlotting/PlottingTest.m
+++ Testing/TestPlotting/PlottingTest.m
@@ -1,6 +1,8 @@
 clear all
 clc
 
+%% Single Phase-Lines
+
         %%% Structure (for ed_kane_mele) %%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % obs{1}    Density (at half filling = 1)                   %
         % obs{2}    Docc (Double Occupancy \in [0,1])               %
@@ -19,12 +21,9 @@ clc
         % obs{15}   Wph (??????)                                    %
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-        % Select observable (iOBS==0 means everything)
+% Select observable (iOBS==0 means everything)
+iOBS = 11;
 
-        iOBS = 11;
-
-% We don't have a SOI-values list, but we can obtain that by just
-% inspecting the subdirectories...
 [SOI_list, SOI_names] = get_list('SOI');
 Nlines = length(SOI_list);
 for iSOI = 1:Nlines
@@ -45,4 +44,116 @@ for iSOI = 1:Nlines
     end
     cd('..'); fprintf('..DONE!\n\nPress any key to continue\n\n'); pause
 end
+
+%% Full Phase Diagram | Just one channel
+
+varID = 5;  % \in [1,15]
+
+[SOI_list, SOI_names] = get_list('SOI');
+Nlines = length(SOI_list);
+phaseVAR = cell(Nlines,1);
+for iSOI = 1:Nlines
+    lineID = SOI_names(iSOI);
+    cd(lineID);
+    clear('ids','obs','U_list');
+    load('observables_line.mat','ids','obs','U_list');
+    if iSOI==1
+        figure("Name",ids{varID});
+        ax = axes;
+    end
+    % Get the line data
+    U = U_list;
+    SOI = SOI_list(iSOI)*ones(length(U),1);
+    phaseVAR{iSOI} = obs{varID};
+    color = phaseVAR{iSOI};
+    Sup = scatter(ax,SOI,U,100,color,'filled','MarkerFaceAlpha',1);
+    hold on
+    cd('..');
+end
+% Title, legend, all of that
+title(ax,ids{varID});
+xlabel(ax,'\lambda_{SO} / t');
+ylabel(ax,'U / t');
+ax.Box = 'on';
+colormap(ax,'copper');
+cb = colorbar(ax);
+clc
+
+%% Full Phase Diagram | Spin-Resolved
+
+varID = 'spinDensity';  % 'Z' | 'ImSigma' | 'spinDensity'
+
+[SOI_list, SOI_names] = get_list('SOI');
+Nlines = length(SOI_list);
+phaseVARup = cell(Nlines,1); phaseVARdown = phaseVARup;
+figure("Name",varID);
+% Axes for spinUP
+axUP = axes;
+for iSOI = 1:Nlines
+    lineID = SOI_names(iSOI);
+    cd(lineID);
+    clear('ids','obs','U_list');
+    load('observables_line.mat','ids','obs','U_list');
+    if strcmp(varID,'Z')==1
+        iOBS = 10;
+    elseif strcmp(varID,'ImSigma')==1
+        iOBS = 12;
+    elseif strcmp(varID,'spinDensity')==1
+        iOBS = 3;
+    else
+        error('Invalid VAR!');
+    end
+    % Get the line data
+    U = U_list;
+    SOI = SOI_list(iSOI)*ones(length(U),1);
+    phaseVARup{iSOI} = obs{iOBS};
+    colorUP = abs(phaseVARup{iSOI});
+    Sup = scatter(axUP,SOI,U,100,colorUP,'filled','MarkerFaceAlpha',1);
+    hold on
+    cd('..');
+end
+% Axes for spinDW
+axDW = axes;
+for iSOI = 1:Nlines
+    lineID = SOI_names(iSOI);
+    cd(lineID);
+    clear('ids','obs','U_list');
+    load('observables_line.mat','ids','obs','U_list');
+    if strcmp(varID,'Z')==1
+        iOBS = 10;
+    elseif strcmp(varID,'ImSigma')==1
+        iOBS = 12;
+    elseif strcmp(varID,'spinDensity')==1
+        iOBS = 3;
+    else
+        error('Invalid VAR!');
+    end
+    % Get the line data
+    U = U_list;
+    SOI = SOI_list(iSOI)*ones(length(U),1);
+    phaseVARdown{iSOI} = obs{iOBS+1};
+    colorDW = abs(phaseVARdown{iSOI});
+    Sdw = scatter(axDW,SOI,U,100,colorDW,'filled','MarkerFaceAlpha',.37);
+    hold on
+    cd('..');
+end
+% Link axes together
+linkaxes([axUP,axDW])
+% Hide the top axes
+axDW.Visible = 'off';
+axDW.XTick = [];
+axDW.YTick = [];
+% Give each one its own colormap
+colormap(axUP,'autumn')
+colormap(axDW,'winter')
+% Then add colorbars and get everything lined up
+set([axUP,axDW],'Position',[.17 .11 .685 .815]);
+cbUP = colorbar(axUP,'Position',[.04 .11 .0675 .815]);
+cbDW = colorbar(axDW,'Position',[.88 .11 .0675 .815]);
+% Title, legend, all of that
+title(axUP,varID);
+xlabel(axUP,'\lambda_{SO} / t');
+ylabel(axUP,'U / t');
+axUP.Box = 'on';
+clc
 
