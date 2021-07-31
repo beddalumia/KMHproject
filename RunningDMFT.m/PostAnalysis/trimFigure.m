02f48e99f4dce5641a7326c2965634c9cc834322
Bellomia <gbellomia@live.it>
2021-07-31 20:28:01 +0200

    Add trimFigure.m
    useful script to trim the white borders of the figure when exporting to PDF.

---
+++ RunningDMFT.m/PostAnalysis/trimFigure.m
@@ -0,0 +1,12 @@
+function trimFigure(inFig)
+%TRIMFIGURE Trims the white borders of the figure when exporting to PDF
+
+if nargin < 1
+    inFig = gcf;
+end
+
+set(inFig,'PaperPositionMode','auto');
+figpos = get(inFig,'PaperPosition');
+set(inFig,'PaperSize',[figpos(3) figpos(4)]);
+end
+
