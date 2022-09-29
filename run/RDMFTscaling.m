82ee6e698aa9d9f3cb0b48a10810ad482401e0de
Gabriele Bellomia <gbellomia@live.it>
2022-09-29 17:40:01 +0200

    Scripts for RDMFT scaling of KM nanoflakes

---
+++ run/RDMFTscaling.m
@@ -0,0 +1,47 @@
+driver = 'ed_kane_mele_flakes';
+doMPI = true;
+
+% SETUP ARRAY OF FLAKE-SIZE VALUES
+flake_size = 1:1:10;
+Nsize = length(flake_size);
+
+% GET SLURM ARRAY TASK ID (if undefined would be NaN)
+aID  = str2double(getenv('SLURM_ARRAY_TASK_ID'));
+
+if isnan(aID)
+    % COMPUTE ALL LINES -> 1:Nsize
+    iSTART = 1;
+    iSTOP  = Nsize;
+else
+    % COMPUTE SIZE(aID) ONLY -> aID:aID
+    iSTART = aID;
+    iSTOP  = aID;
+end
+
+%% SOC-Line: outer loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for iSIZE = iSTART:iSTOP
+
+    R  = flake_size(iSIZE);    % Input Spin-Orbit 
+
+    rDIR= sprintf('R=%f',R);   % Make a folder named 'R=...', where '...'
+    mkdir(rDIR);               % is the given value for SpinOrb interaction
+    cd(rDIR);                  % Enter the R-folder
+
+    copyfile ../input*         % Copy inside the **external** input file
+
+    %% Phase-Line: inner loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    Uold    = NaN;			% Restart point
+    Umin    = 0.00;         % Phase-line iSTART
+    Ustep   = 0.10;			% Phase-line step
+    Umax    = 10.0;         % Phase-line end
+
+    runDMFT.dry_line(driver,doMPI,Uold,Umin,Ustep,Umax,'radius',R,'wmixing',0.25);
+
+    %runDMFT.refresh_line(driver,doMPI,100,true,'bath_type','replica','wmixing',0.1)
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    cd ..                          % Exit the R-folder
+
+end
---
+++ run/hpc_template/rdmft.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+#
+#
+# ==== SLURM part (resource manager part) ===== #
+#
+# ---- Metadata configuration ----
+#
+#SBATCH --job-name=KMH.flake                # The name of your job, you'll se it in squeue.
+#SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
+#SBATCH --mail-user=gbellomi@sissa.it       # Where to send the mail
+#
+# ---- CPU resources configuration  ----    |  Clarifications at https://slurm.schedmd.com/mc_support.html
+#
+#SBATCH --cpus-per-task=40                  # Number of threads per MPI rank (MAX: 2x32 cores on _partition_2, 2x20 cores on _partition_1) 
+#SBATCH --nodes=3                           # Number of nodes
+#
+# ---- Memory configuration ----
+#
+#SBATCH --mem=0                             # Memory per node (MAX: 63500 on the new ones, 40000 on the old ones); incompatible with --mem-per-cpu.
+#
+# ---- Partition, Walltime and Output ----
+#
+#SBATCH --array=01-10                       # Create a job array. Useful for multiple, similar jobs. To use, read this: https://slurm.schedmd.com/job_array.html
+#SBATCH --partition=long1                   # Partition (queue). Avail: regular1, regular2, long1, long2, wide1, wide2, gpu1, gpu2. Multiple partitions are possible.
+#SBATCH --time=24:00:00                     # Time limit hrs:min:sec
+#SBATCH --output=sLOG_%x_out%A_%a.txt       # Standard output log -- WARNING: %x requires a new enough SLURM. Use %j for regular jobs and %A-%a for array jobs
+#SBATCH --error=sLOG_%x_err%A_%a.txt        # Standard error  log -- WARNING: %x requires a new enough SLURM. Use %j for regular jobs and %A-%a for array jobs
+#
+#
+# ==== Modules part (load all the modules) ===== #
+#
+# ---- ITCS-mantained modules ----
+#
+module load gnu8/8.3.0
+module load mkl/19.1.3.304
+module load openmpi3/3.1.4
+module load matlab/2021a
+module load python3/3.6
+module load gnuplot/5.2.4
+#
+# ---- QcmPlab stuff ----
+#
+#module load scifor/gnu
+#module load dmft_tools/gnu
+#module load dmft_ed/gnu
+#
+#
+# ==== Info part (say things) ===== #
+#
+# > DO NOT MODIFY. This part prints useful info on your output file.
+#
+NOW=`date +%H:%M-%a-%d/%b/%Y`
+echo '------------------------------------------------------'
+echo 'This job is allocated on '$SLURM_JOB_CPUS_PER_NODE' cpu(s)'
+echo 'Job is running on node(s): '
+echo  $SLURM_JOB_NODELIST
+echo '------------------------------------------------------'
+echo 'WORKINFO:'
+echo 'SLURM: job starting at           '$NOW
+echo 'SLURM: sbatch is running on      '$SLURM_SUBMIT_HOST
+echo 'SLURM: executing on cluster      '$SLURM_CLUSTER_NAME
+echo 'SLURM: executing on partition    '$SLURM_JOB_PARTITION
+echo 'SLURM: working directory is      '$SLURM_SUBMIT_DIR
+echo 'SLURM: current home directory is '$(getent passwd $SLURM_JOB_ACCOUNT | cut -d: -f6)
+echo ""
+echo 'JOBINFO:'
+echo 'SLURM: job identifier is         '$SLURM_JOBID
+echo 'SLURM: job name is               '$SLURM_JOB_NAME
+echo ""
+echo 'NODEINFO:'
+echo 'SLURM: number of nodes is        '$SLURM_JOB_NUM_NODES
+echo 'SLURM: number of cpus/node is    '$SLURM_JOB_CPUS_PER_NODE
+echo 'SLURM: number of gpus/node is    '$SLURM_GPUS_PER_NODE
+echo '------------------------------------------------------'
+#
+# ==== End of Info part (say things) ===== #
+#
+cd $SLURM_SUBMIT_DIR # Brings the shell into the directory from which you’ve submitted the script.
+#
+# ==== JOB COMMANDS ===== #
+#
+# > The part that actually executes all the operations you want to do.
+#   Just fill this part as if it was a regular Bash script that you want to
+#   run on your computer.
+#
+# >> RDMFT-Workflow (fill and uncomment just one of these...)
+matlab -batch RDMFTscaling
+#matlab -batch RefreshDMFTgrid
+#
+#
+# ==== END OF JOB COMMANDS ===== #
+#
+#
+# Wait for processes, if any.
+echo "Waiting for all the processes to finish..."
+wait
+
+
