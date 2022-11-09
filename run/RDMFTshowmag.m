c46710a69fa3059fadf66753bcf455701886f338
Gabriele Bellomia <gbellomi@sissa.it>
2022-11-09 20:00:43 +0100

    Intermediate
    Trying to move away from bubblechart plots, I'll explain later the ratio
    and give all the details.

--- run/RDMFTshowmag.m
+++ run/RDMFTshowmag.m
@@ -45,13 +45,50 @@ function h = single_frame(U,lattice_file)
 
     % Represent as double bubblechart
     h = figure('Name',"U="+string(U));
-    bubblechart(x(z>0),y(z>0),sqrt(z(z>0)),rgb.xkcd('red'));
+    %bubblechart(x(z>0),y(z>0),sqrt(z(z>0)),rgb.xkcd('red'));
+    draw_lattice(x(z>0),y(z>0),sqrt(z(z>0)),FaceColor=rgb.xkcd('red'));
     hold on
-    bubblechart(x(z<0),y(z<0),sqrt(-z(z<0)),rgb.xkcd('blue'));
+    %bubblechart(x(z<0),y(z<0),sqrt(-z(z<0)),rgb.xkcd('blue'));
+    draw_lattice(x(z<0),y(z<0),-sqrt(z(z<0)),FaceColor=rgb.xkcd('blue'));
     axis equal
     axis off
     title("U="+string(U));
     
     % What to do about xlim and ylim??
+    currentunits = get(gca,'Units');
+    set(gca, 'Units', 'Points');
+    axpos = get(gca,'Position');
+    set(gca, 'Units', currentunits);
+    markerWidth = s/diff(xlim)*axpos(3); % Calculate Marker width in points
+    set(h, 'SizeData', markerWidth^2)
 
+end
+
+function sites = draw_lattice(x,y,r,varargin)
+    %% Draws a lattice, with site dimensions given in proper x,y units
+    %  i.e. rescalign the figure would not change the aspect ratio, as
+    %  the circles must maintain their dimensions in the rescaled x,y
+    %  axes. This way we can ensure that, no matter what screensize or
+    %  system specs (HPC, laptop, whatever) the sites do not overlap.
+    %
+    %  >> lattice = draw_site(x,y,r,varargin) 
+    %
+    %   x :: a 1d array of site coordinates (x axis)
+    %   y :: a 1d array of site coordinates (y axis)
+    %   r :: a 1d array of values for the site radii (hint: data ∝ area!)
+    %
+    %   lattice :: a cell array of handles to all the rectangles (to allow 
+    %              OOP-style selection of properties, with granularity)
+    %              > you can use the varargin to pass global settings, as
+    %                keyargs or name-value pairs.
+    %
+    %  See also rectangle
+    N = length(r);
+    sites = cell(N,1);
+    for i = 1:N
+        pos = [x(i),y(i),r(i),r(i)];
+        sites{i} = rectangle('Position',pos,'Curvature',[1 1],varargin{:});
+        hold on
+    end
+    hold off
 end
