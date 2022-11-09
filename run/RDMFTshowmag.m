b2c407176fedf967cb87d097816f35f9410f82f4
Gabriele Bellomia <gbellomi@sissa.it>
2022-11-09 16:43:53 +0100

    First try at a script that would display (or animate) RDMFT observables
    It would become a function in +plotDMFT eventually, I guess, but let's
    wait until is general enough for that.

---
+++ run/RDMFTshowmag.m
@@ -0,0 +1,57 @@
+Ulist = postDMFT.get_list('U');
+lattice_file = 'flakes.txt';
+gif_name = 'animation.gif';
+plotDMFT.import_colorlab;
+
+if not(isempty(Ulist))
+    for iU = 1:length(Ulist)
+        
+        U = Ulist(iU);
+        UDIR= sprintf('U=%f',U);
+        if ~isfolder(UDIR)
+            errstr = 'U_list appears to be inconsistent: ';
+            errstr = [errstr,UDIR];
+            errstr = [errstr,' folder has not been found.'];
+            error(errstr);
+        end
+        cd(UDIR);
+        
+        frame = single_frame(U,lattice_file);
+        push_frame(gifname,iU,length(Ulist),0.1,frame);
+        close(frame)
+        
+        cd ..
+        
+    end
+else
+    % If Ulist is empty then we are inside a Udir already and want just a frame
+    [~,Udir,decimals] = fileparts(pwd); % hate this
+    Udir = [Udir,decimals];
+    Uval = sscanf(Udir,'%f');
+    frame = single_frame(Uval,lattice_file);
+end
+
+function h = single_frame(U,lattice_file)
+
+    % POSIX dependent, sorry
+    system("awk '{print $5}' observables_last_ineq0* > magX.txt");
+    z = load('magX.txt');
+    delete('magX.txt');
+
+    % Load coordinates
+    lattice = load(lattice_file);
+    x = lattice(:,1);
+    y = lattice(:,2);
+
+    % Represent as double bubblechart
+    h = figure('Name',"U="+string(U));
+    bubblechart(x(z>0),y(z>0),sqrt(z(z>0)),rgb.xkcd('red'));
+    hold on
+    bubblechart(x(z<0),y(z<0),sqrt(-z(z<0)),rgb.xkcd('blue'));
+    axis equal
+    axis off
+    title("U="+string(U));
+    
+    % What to do about xlim and ylim??
+
+end
