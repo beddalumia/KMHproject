daffcfdba3032ba758008bd543760a454349b072
Gabriele Bellomia <gbellomia@live.it>
2023-11-09 07:19:51 +0100

    Add a post_strawberry script to extract the bulk Z2 value
    We try to round the mode of the marker. For big enough flakes it should
    work, for small ones maybe not (but arguably, in those cases, there is
    no real concept of bulk).

---
+++ run/post_strawberry.m
@@ -0,0 +1,31 @@
+TOPLEVEL = pwd;
+
+[~,rdirs] = QcmP.post.get_list('R');
+
+for r = 1:length(rdirs)
+
+    fprintf("    > %s\n",rdirs(r))
+    cd(rdirs(r))
+
+    [~,udirs] = QcmP.post.get_list('U');
+
+    for u = 1:length(udirs)
+
+        fprintf("        >> %s\n",udirs(u))
+        cd(udirs(u))
+
+        try
+            load('Z2marker.txt')
+        catch
+            Z2marker = NaN;
+        end
+        Z2bulk = round(mode(Z2marker));
+        writematrix(Z2bulk);
+
+        cd('..')
+
+    end
+
+    cd('..')
+
+end
